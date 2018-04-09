6814372d52b5720b0ff155461cedae88a0e81bf7
minhquan94 <minhquan9402@gmail.com>
2018-04-10 00:40:40 +0700

    add toolbar component

--- WebBlog/web-angular/src/app/app.module.ts
+++ WebBlog/web-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './monitor/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
